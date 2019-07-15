fa6d9b7469db985d599474db1de83dbe3c65320d
philzhang01 <philzhang01@gmail.com>
2019-07-15 00:19:38 -0700

    add redux and update js to jsx

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 
-import MenuItem from "../menu-item/menu-item";
+import MenuItem from "../menu-item/menu-item.component";
 
-import "./directory.scss";
+import "./directory.styles.scss";
 
 class Directory extends React.Component {
   constructor() {
