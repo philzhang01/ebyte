1317755dd288d563e94d031128ed78729576777b
philzhang01 <philzhang01@gmail.com>
2019-07-14 21:37:14 -0700

    add sign up

--- src/components/sign-in/sign-in.js
+++ src/components/sign-in/sign-in.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 import FormInput from "../form-input/form-input";
-import CustomButton from "../custom-buttom/custom-buttom";
+import CustomButton from "../custom-button/custom-button";
 
 import { signInWithGoogle } from "../../firebase/firebase.utils";
 
